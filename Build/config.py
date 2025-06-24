858cef5cf45dcb36e4a2f6c31c137ae62702a166
DeepthiR <115728483+DeepthiR2405@users.noreply.github.com>
2025-06-24 14:43:17 +0530

    Update config.py

---
+++ Build/config.py
@@ -0,0 +1,7 @@
+# config.py
+MYSQL_HOST = "localhost"
+MYSQL_USER = "root"
+MYSQL_PASSWORD = ""  # Set your MySQL password
+MYSQL_DB = "certificate_system"
+MYSQL_CURSORCLASS = "DictCursor"
+SECRET_KEY = "your_secret_key"  # Change this to a strong secret key
